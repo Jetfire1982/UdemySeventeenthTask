5722b44452d64c126651bccdd7e5e82392b0b7b9
Petr <Mclaren-f1@rambler.ru>
2020-04-17 13:29:15 +0300

    teacher's version

--- script.js
+++ script.js
@@ -1,45 +1,52 @@
-$(document).ready(function () {
-    $('.main_btna').on('click', function () {
+// $(document).ready(function () {
+//     jQuery('.main_btna').on('click', function () {
 
-        $('[class = overlay]').fadeIn('low');
-        $('.modal').animate({
-            opacity: 'toggle',
-            height: 'toggle'
-        }, 1000);
+//         $('[class = overlay]').fadeIn('low');
+//         $('.modal').animate({
+//             opacity: 'toggle',
+//             height: 'toggle'
+//         }, 1000);
+//     });
+
+//     $('.main_btn').on('click', function () {
+
+//         $('[class = overlay]').fadeIn('low');
+//         $('.modal').animate({
+//             opacity: 'toggle',
+//             height: 'toggle'
+//         }, 1000);
+//     });
+
+//     $('[href="#sheldure"]').on('click', function () {
+
+//         $('[class = overlay]').fadeIn('low');
+//         $('.modal').animate({
+//             opacity: 'toggle',
+//             height: 'toggle'
+//         }, 1000);
+//     });
+
+//     $('.modal > button').on('click', function () {
+//         $('[class = overlay]').fadeOut('low');
+//         $('.modal').animate({
+//             opacity: 'toggle',
+//             height: 'toggle'
+//         }, 1000);
+//         console.log('куку');
+//     });
+
+// });
+
+$(document).ready(function() {
+
+    $('.main_btna, .main_btn, a[href="#sheldure"]').click(function() {
+        $('.overlay').fadeIn(1000);
+        $('.modal').slideDown(1000);
     });
 
-    $('.main_btn').on('click', function () {
-
-        $('[class = overlay]').fadeIn('low');
-        $('.modal').animate({
-            opacity: 'toggle',
-            height: 'toggle'
-        }, 1000);
+    $('.close').click(function() {
+        $('.modal').slideUp(1000);
+        $('.overlay').fadeOut(1000);
     });
-
-    $('[href="#sheldure"]').on('click', function () {
-
-        $('[class = overlay]').fadeIn('low');
-        $('.modal').animate({
-            opacity: 'toggle',
-            height: 'toggle'
-        }, 1000);
-    });
-
-    $('.modal > button').on('click', function () {
-        $('[class = overlay]').fadeOut('low');
-        $('.modal').animate({
-            opacity: 'toggle',
-            height: 'toggle'
-        }, 1000);
-        console.log('куку');
-    });
-
-
-    // let x=document.querySelector('#tour');
-    // let x1=document.querySelector('.overlay');
-    // console.log(x);
-    // console.log(x1);
-    // $('[class = content]')
 
 });
